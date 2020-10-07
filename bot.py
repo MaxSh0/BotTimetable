7f6ef58a41d96f49d7a2b5e038513f100598a05b
Maxim Shakurov <m.shakurov99@mail.ru>
2020-10-07 19:47:52 +0400

    Добавлены кнопки

--- bot.py
+++ bot.py
@@ -12,13 +12,14 @@ def Today():
     now = datetime.datetime.now()
     i = 0
     for Day in text:
+        textDay = re.sub(r'\d+','',Day,flags=re.UNICODE)
         for CurDate in text[Day]['Date']:
             if (now.strftime("%d-%m-%Y") == CurDate):
                 TextSubject = "\n";
                 for Subject in text[Day]['Subjects']:
                     TextSubject = TextSubject + Subject + "  " + text[Day]['Time'][i] + "\n"
                     i += 1
-                fulltext = "Дата: " + CurDate + "\nПредметы: " + TextSubject
+                fulltext = textDay+"\nДата: " + CurDate + "\nПредметы: " + TextSubject
     return fulltext
 
 def Tomorrow():
@@ -28,13 +29,14 @@ def Tomorrow():
     tomorrow = today + datetime.timedelta(days=1)
     i = 0
     for Day in text:
+        textDay = re.sub(r'\d+', '', Day, flags=re.UNICODE)
         for CurDate in text[Day]['Date']:
             if (tomorrow.strftime("%d-%m-%Y") == CurDate):
                 TextSubject = "\n";
                 for Subject in text[Day]['Subjects']:
                     TextSubject = TextSubject + Subject + "  " + text[Day]['Time'][i] + "\n"
                     i += 1
-                fulltext = "Дата: " + CurDate + "\nПредметы: " + TextSubject
+                fulltext = textDay+"\nДата: " + CurDate + "\nПредметы: " + TextSubject
     return fulltext
 
 def All():
@@ -74,23 +76,32 @@ def definition_of_the_week():
 
 @bot.message_handler(commands=['start'])
 def welcome(massage):
-    bot.send_message(massage.chat.id, "Расписание 4.1 курс")
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttonToday = telebot.types.KeyboardButton('Сегодня')
+    buttonTomorrow = telebot.types.KeyboardButton('Завтра')
+    buttonAll = telebot.types.KeyboardButton('Общее')
+    markup.add(buttonToday,buttonTomorrow,buttonAll)
+    bot.send_message(massage.chat.id, "Расписание 4.1 курс", reply_markup=markup)
+
+
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    if(re.search(r'(П|п)ривет|(З|з)дравствуй|(Х|х)ей|(Х|х)елло|(H|h)ello',message.text) != None):
-        bot.send_message(message.chat.id, "Ну здравствуй, сталкер")
-    elif(re.search(r'(h|H)elp|(п|П)омощь|(П|п)омоги',message.text) !=None):
-        bot.send_message(message.chat.id, "Команды:\nСегодня\nЗавтра\nНеделя\nОбщее")
-    elif(re.search(r'(С|с)егодня',message.text) !=None):
-        bot.send_message(message.chat.id, Today())
-    elif(re.search(r'(З|з)автра',message.text) !=None):
-        bot.send_message(message.chat.id, Tomorrow())
-    elif(re.search(r'(О|о)бщее',message.text) !=None):
-        bot.send_message(message.chat.id, All())
-    else:
-        bot.send_message(message.chat.id, "Ключевые слова не найдены")
+    if message.chat.type == 'private':
+        if(re.search(r'(П|п)ривет|(З|з)дравствуй|(Х|х)ей|(Х|х)елло|(H|h)ello',message.text) != None):
+            bot.send_message(message.chat.id, "Ну здравствуй, сталкер")
+        elif(re.search(r'(h|H)elp|(п|П)омощь|(П|п)омоги',message.text) !=None):
+            bot.send_message(message.chat.id, "Команды:\nСегодня\nЗавтра\nНеделя\nОбщее")
+        elif(re.search(r'(С|с)егодня',message.text) !=None):
+            bot.send_message(message.chat.id, Today())
+        elif(re.search(r'(З|з)автра',message.text) !=None):
+            bot.send_message(message.chat.id, Tomorrow())
+        elif(re.search(r'(О|о)бщее',message.text) !=None):
+            bot.send_message(message.chat.id, All())
+        else:
+            bot.send_message(message.chat.id, "Ключевые слова не найдены")
+
 
 
 
